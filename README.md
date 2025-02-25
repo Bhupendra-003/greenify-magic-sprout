ac099b2b200c984e0e471517e00955c14f7ec609
= <=>
2025-02-25 18:23:25 +0530

    initial commit

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Project info
 
-**URL**: https://lovable.dev/projects/0296b130-8821-4111-b0f1-4d1446b0bfa8
+**URL**: https://lovable.dev/projects/647d09cf-abd6-4690-9a05-fa566b7a6850
 
 ## How can I edit this code?
 
@@ -10,7 +10,7 @@ There are several ways of editing your application.
 
 **Use Lovable**
 
-Simply visit the [Lovable Project](https://lovable.dev/projects/0296b130-8821-4111-b0f1-4d1446b0bfa8) and start prompting.
+Simply visit the [Lovable Project](https://lovable.dev/projects/647d09cf-abd6-4690-9a05-fa566b7a6850) and start prompting.
 
 Changes made via Lovable will be committed automatically to this repo.
 
@@ -62,7 +62,7 @@ This project is built with .
 
 ## How can I deploy this project?
 
-Simply open [Lovable](https://lovable.dev/projects/0296b130-8821-4111-b0f1-4d1446b0bfa8) and click on Share -> Publish.
+Simply open [Lovable](https://lovable.dev/projects/647d09cf-abd6-4690-9a05-fa566b7a6850) and click on Share -> Publish.
 
 ## I want to use a custom domain - is that possible?
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CitizenDashboard from "./pages/CitizenDashboard";
+import NGODashboard from "./pages/NGODashboard";
+import ReportIssue from "./pages/ReportIssue";
 import NotFound from "./pages/NotFound";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 const queryClient = new QueryClient();
 
@@ -14,11 +22,37 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/citizen/*"
+              element={
+                <PrivateRoute userType="citizen">
+                  <Routes>
+                    <Route path="dashboard" element={<CitizenDashboard />} />
+                    <Route path="report" element={<ReportIssue />} />
+                    <Route path="*" element={<Navigate to="dashboard" />} />
+                  </Routes>
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/ngo/*"
+              element={
+                <PrivateRoute userType="ngo">
+                  <Routes>
+                    <Route path="dashboard" element={<NGODashboard />} />
+                    <Route path="*" element={<Navigate to="dashboard" />} />
+                  </Routes>
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
